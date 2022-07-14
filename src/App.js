95efbed73a9b3d34f8cecc35a9316f08551b0cd4
NguyenQuocCuong98 <anhyeunguyen1998@gmail.com>
2022-07-14 15:30:42 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Xin chào tất cả anh em</h1>
+    </div>
+  );
+}
+
+export default App;
